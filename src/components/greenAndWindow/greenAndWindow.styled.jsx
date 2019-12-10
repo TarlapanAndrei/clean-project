69c764b127e1f7578d7655a23971a63527c8631a
Andrei Tarlapan <Tarlapan@gmail.com>
2019-12-11 01:51:21 +0200

    add homepage, @medi, redux

--- src/components/greenAndWindow/greenAndWindow.styled.jsx
+++ src/components/greenAndWindow/greenAndWindow.styled.jsx
@@ -21,6 +21,9 @@ export const GreenAndWindowSquare = styled.div`
     width: 100%;
     heigth: 80%;
   }
+  @media screen and (max-width: 414px){
+    max-height: 500px;
+  }
 `;
 export const GreenAndWindowImage = styled.div`
   display: flex;
@@ -33,23 +36,100 @@ export const GreenAndWindowImage = styled.div`
     width: 100%;
     background-size: cover;
   }
+  @media screen and (max-width: 414px){
+    height: 500px;
+  }
 `;
 export const GreenWindowTextContainer = styled.div`
   margin-top: 80px;
   color: white;
   display: flex;
   flex-direction: column;
+  @media screen and (max-width: 1365px){
+    margin-top: 70px;
+  }
+  @media screen and (max-width: 1099px){
+    margin-top: 60px;
+  }
 `;
 export const Hdiv = styled.div`
     margin-left: 30px;
     h1{
       font-size: 62px;
     }
+    @media screen and (max-width: 1365px){
+      margin-left: 25px;
+      h1{
+        font-size: 55px;
+      }
+    }
+    @media screen and (max-width: 1099px){
+      margin-left: 20px;
+      h1{
+        font-size: 45px;
+      }
+    }
+    @media screen and (max-width: 949px){
+      h1{
+        font-size: 36px;
+      }
+    }
+    @media screen and (max-width: 879px){
+      h1{
+        font-size: 32px;
+      }
+    }
+    @media screen and (max-width: 812px){
+      h1{
+        font-size: 25px;
+      }
+    }
+    @media screen and (max-width: 666px){
+      h1{
+        font-size: 21px;
+        margin-left: 10px;
+      }
+    }
+    @media screen and (max-width: 639px){
+      h1{
+        font-size: 19px;
+        margin-left: 10px;
+      }
+    }
+    @media screen and (max-width: 414px){
+      h1{
+        font-size: 25px;
+        margin-left: 10px;
+    }
 `;
 export const Pdiv = styled.div`
   margin-left: 30px;
   margin-right: 25px;
   font-size: 25px;
+  @media screen and (max-width: 1365px){
+    margin-left: 25px;
+    margin-right: 20px;
+    font-size: 20px;
+  }
+  @media screen and (orientation : portrait) and (max-width: 1024px){
+    font-size: 22px;
+  }
+  @media screen and (max-width: 812px){
+    font-size: 20px;
+  }
+  @media screen and (max-width: 666px){
+    font-size: 18px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  @media screen and (max-width: 639px){
+    font-size: 16px;
+  }
+  @media screen and (max-width: 414px){
+    margin-left: 30px;
+    margin-right: 25px;
+    font-size: 22px;
+  }
 `;
 export const ButtonDiv = styled.div`
   width: 32%;
@@ -58,8 +138,65 @@ export const ButtonDiv = styled.div`
   margin-top: 20px;
   border: 2px solid white;
   border-radius: 6px;
+  @media screen and (max-width: 1365px){
+    margin-left: 25px;
+    margin-top: 17px;
+  }
+  @media screen and (max-width: 1099px){
+    width: 36%;
+    height: 9%;
+  }
+  @media screen and (orientation : portrait) and (max-width: 1024px){
+    width: 42%;
+    height: 11%;
+  }
+  @media screen and (max-width: 949px){
+    width: 46%;
+    height: 11%;
+  }
+  @media screen and (max-width: 879px){
+    width: 49%;
+  }
+  @media screen and (max-width: 812px){
+    width: 54%;
+  }
+  @media screen and (max-width: 735px){
+    width: 59%;
+  }
+  @media screen and (max-width: 666px){
+    width: 65%;
+  }
+  @media screen and (max-width: 639px){
+    width: 69%;
+  }
+  @media screen and (max-width: 579px){
+    width: 77%;
+  }
+  @media screen and (max-width: 414px){
+    width: 87%;
+  }
 `;
 export const ButtonText = styled.div`
   margin: 11px 20px;
   font-size: 23px;
+  @media screen and (max-width: 1365px){
+    margin: 7px 18px;
+    font-size: 20px;
+  }
+  @media screen and (max-width: 1199px){
+    font-size: 17px;
+  }
+  @media screen and (orientation : portrait) and (max-width: 1024px){
+    font-size: 20px;
+  }
+  @media screen and (max-width: 414px){
+    margin: 10px 75px;
+    font-size: 25px;
+  }
+  @media screen and (max-width: 375px){
+    margin: 6px 55px;
+  }
+  @media screen and (max-width: 360px){
+    margin: 6px 55px;
+  }
 `;
--- src/pages/homePage/HomePage.styled.jsx
+++ src/pages/homePage/HomePage.styled.jsx
@@ -8,24 +8,32 @@ export const HomepageContainer = styled.div`
   height: 2800px;
   position: relative;
   margin: 0px auto;
-  @media screen and (max-width: 1400px){
+  @media screen and (orientation:landscape) and (max-width: 1366px){
+    max-width: 1366px;
+    height: 2710px;
+    margin: 0 3px;
+  }
+  @media screen and (max-width: 1365px){
+    max-width: 1300px;
+    height: 2600px;
+  }
+  @media screen and (max-width: 1299px){
+    max-width: 1200px;
+    height: 2400px;
+  }
+  @media screen and (max-width: 1199px){
     max-width: 1100px;
     height: 2200px;
   }
-  @media screen and (orientation:landscape) and (max-width: 1366px){
-    max-width: 1362px;
-    height: 2610px;
-    margin: 0 5px;
-  }
-  @media screen and (max-width: 1360px){
-    max-width: 1100px;
+  @media screen and (max-width: 1099px){
+    max-width: 1025px;
     height: 2200px;
   }
 
   @media screen and (orientation:landscape) and (max-width: 1024px){
     max-width: 1024px;
     height: 2000px;
-    margin: 0 5px;
+    margin: 0 5px; 
   }
   @media screen and (orientation : portrait) and (max-width: 1024px){
     max-width: 1024px;
@@ -33,53 +41,75 @@ export const HomepageContainer = styled.div`
     margin: 0 5px;
   }
   @media screen and (max-width: 1023px){
-    max-width: 880px;
-    height: 1800px;
+    max-width: 950px;
+    height: 1900px;
     margin: 0 5px;
   }
-  @media screen and (max-width: 880px){
-    max-width: 824px;
+  @media screen and (max-width: 949px){
+    max-width: 880px;
+    height: 1600px;
+    margin: 0 5px;
+
+    @media screen and (orientation:landscape) and (max-width: 879px){
+      max-width: 813px;
+      height: 1630px;
+      margin: 0 5px;
+    }
+  
+  @media screen and (max-width: 812px){
+    max-width: 812px;
     height: 1300px;
     margin: 0 5px;
   }
-  @media screen and (orientation:landscape) and (max-width: 823px){
-    max-width: 823px;
-    height: 1630px;
-    margin: 0 5px;
-  }
-  @media screen and (max-width: 822px){
-    max-width: 768px;
+  @media screen and (max-width: 811px){
+    max-width: 769px;
     height: 1300px;
     margin: 0 5px;
   }
   @media screen and (orientation : portrait) and (max-width: 768px){
-    max-width: 764px;
+    max-width: 768px;
     height: 1510px;
     margin: 0 5px;
   }
-  @media screen and (orientation:landscape) and (max-width: 750px){
-    max-width: 725px;
-    height: 1500px;
-    margin: 0 5px;
-  }
-  @media screen and (orientation:landscape) and (max-width: 675px){
-    max-width: 660px;
+  @media screen and (max-width: 767px){
+    max-width: 732px;
     height: 1300px;
     margin: 0 5px;
   }
-  @media screen and (max-width: 650px){
-    max-width:500px;
+  @media screen and (orientation : portrait) and (max-width: 731px){
+    max-width: 731px;
+    height: 1500px;
+    margin: 0 5px;
+  }
+  @media screen and (orientation:landscape) and (max-width: 667px){
+    max-width: 667px;
+    height: 1300px;
+    margin: 0 5px;
+  }
+  @media screen and (max-width: 666px){
+    max-width:641px;
     height: 1230px;
     margin: 0 5px;
   }
   @media screen and (orientation:landscape) and (max-width: 640px){
-    max-width: 636px;
+    max-width: 640px;
     height: 1100px;
     margin: 0 5px;
   }
-  @media screen and (max-width: 500px){
-    max-width:496px;
-    height: 3280px;
+  @media screen and (max-width: 639px){
+    max-width:569px;
+    height: 1230px;
+    margin: 0 5px;
+  }
+  @media screen and (max-width: 568px){
+    max-width:568px;
+    height: 1230px;
+    margin: 0 5px;
+    background-color: blue;
+  }
+  @media screen and (max-width: 414px){
+    max-width:414px;
+    height: 4280px;
     margin: 0 5px;
   }
   @media screen and (max-width: 400px){
@@ -87,8 +117,13 @@ export const HomepageContainer = styled.div`
     height: 2880px;
     margin: 0 5px;
   }
-  @media screen and (max-width: 380px){
-    max-width:374px;
+  @media screen and (max-width: 375px){
+    max-width:375px;
+    height: 2880px;
+    margin: 0 5px;
+  }
+  @media screen and (max-width: 360px){
+    max-width:360px;
     height: 2990px;
     margin: 0 5px;
   }
