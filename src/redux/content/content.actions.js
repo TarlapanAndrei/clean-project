761bbd33cdaaf7466e172909272788f9d7c93110
Andrei Tarlapan <Tarlapan@gmail.com>
2019-12-12 00:40:42 +0200

    add rom language and phone call

---
+++ src/redux/content/content.actions.js
@@ -0,0 +1,8 @@
+import contentActionTypes from './content.types';
+
+export const changeLangToRu = () =>({
+  type: contentActionTypes.LANGUAGE_STATUS_RU
+});
+export const changeLangToRo = () =>({
+  type: contentActionTypes.LANGUAGE_STATUS_RO
+});
