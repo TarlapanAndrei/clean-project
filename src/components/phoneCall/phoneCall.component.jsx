c086919890eec9bad6c460643fc2244487d8e916
Andrei Tarlapan <Tarlapan@gmail.com>
2019-12-18 22:53:36 +0200

    add facebook icon for mobile

--- src/components/phoneCall/phoneCall.component.jsx
+++ src/components/phoneCall/phoneCall.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import {PhoneDropDownContainer, DivPhone} from './phoneCall.styled';
 import Phone from '../../assets/phone.png';
 import Viber from '../../assets/viber.png';
+import Facebook from '../../assets/facebook3.png';
 
 const PhoneCall = () =>(
   <PhoneDropDownContainer>
     <DivPhone>
     <a href="tel:+373 (22)288 611"><img src={Phone} alt="phone"/></a>
+    </DivPhone>
+    <DivPhone>
+      <a href="https://www.facebook.com/profile.php?id=100015586867540"><img src={Facebook} alt="facebookIcon"/></a>
     </DivPhone>
     <DivPhone>
     <a href=" https://api.whatsapp.com/send?phone=+37369106651"><img src={Viber} alt="viber" /></a>
